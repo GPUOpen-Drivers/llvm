22be27f56f7272199f9033f0603d248bac0cdd41
David Stuttard <david.stuttard@amd.com>
2017-07-14 14:34:20 +0100

    Buffer memory op merging pass for s_buffer_load
    This pass attempts to merge s_buffer_load_dword instructions into larger
    sizes.
    Constraints are that the resource descriptor, glc flag and size are all the same
    for contiguous load instructions.
    
    TODO: it should be possible to add different instructions to this as well such
    as s_buffer_store and the vector variants buffer_load/buffer_store
    
    V2: Fixes to sbuff-merge test
    V3: Fixed smrd test for smem merging
    
    Change-Id: Ie98107ad3b27b3c8c2da7afc3d129336f924895a

--- lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -175,6 +175,7 @@ extern "C" void LLVMInitializeAMDGPUTarget() {
   initializeSIWholeQuadModePass(*PR);
   initializeSILowerControlFlowPass(*PR);
   initializeSIInsertSkipsPass(*PR);
+  initializeSIBufMemMergePass(*PR);
   initializeSIMemoryLegalizerPass(*PR);
   initializeSIDebuggerInsertNopsPass(*PR);
   initializeSIOptimizeExecMaskingPass(*PR);
@@ -779,6 +780,7 @@ void GCNPassConfig::addMachineSSAOptimization() {
   addPass(&SIFoldOperandsID);
   addPass(&DeadMachineInstructionElimID);
   addPass(&SILoadStoreOptimizerID);
+  addPass(&SIBufMemMergeID);
   if (EnableSDWAPeephole) {
     addPass(&SIPeepholeSDWAID);
     addPass(&EarlyMachineLICMID);
