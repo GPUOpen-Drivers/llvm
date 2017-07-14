845713283658f62abeeece9077db7a873a403372
David Stuttard <david.stuttard@amd.com>
2017-07-14 14:34:20 +0100

    Buffer memory op merging pass for s_buffer_load
    This pass attempts to merge s_buffer_load_dword instructions into larger
    sizes.
    Constraints are that the resource descriptor, glc flag and size are all the same
    for contiguous load instructions.
    
    TODO: it should be possible to add different instructions to this as well such
    as s_buffer_store and the vector variants buffer_load/buffer_store
    
    Change-Id: Ie98107ad3b27b3c8c2da7afc3d129336f924895a

--- lib/Target/AMDGPU/AMDGPU.h
+++ lib/Target/AMDGPU/AMDGPU.h
@@ -47,6 +47,7 @@ FunctionPass *createSIWholeQuadModePass();
 FunctionPass *createSIFixControlFlowLiveIntervalsPass();
 FunctionPass *createSIOptimizeExecMaskingPreRAPass();
 FunctionPass *createSIFixSGPRCopiesPass();
+FunctionPass *createSIBufMemMergePass();
 FunctionPass *createSIMemoryLegalizerPass();
 FunctionPass *createSIDebuggerInsertNopsPass();
 FunctionPass *createSIInsertWaitcntsPass();
@@ -119,6 +120,10 @@ extern char &SILowerI1CopiesID;
 
 void initializeSILoadStoreOptimizerPass(PassRegistry &);
 extern char &SILoadStoreOptimizerID;
+
+void initializeSIBufMemMergePass(PassRegistry &);
+extern char &SIBufMemMergeID;
+  
 
 void initializeSIWholeQuadModePass(PassRegistry &);
 extern char &SIWholeQuadModeID;
