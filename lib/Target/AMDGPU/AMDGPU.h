23bba32c56379c106e967193e1938464843243d4
Carl Ritson <carl.ritson@amd.com>
2019-06-13 14:33:04 +0100

    [AMDGPU] Revised optional bounds checking for scratch accesses
    Implement a pass, enabled by -amdgpu-scratch-bounds-checking,
    or the subtarget feature "enable-scratch-bounds-checks",
    which adds bounds checks to scratch accesses.
    When this pass is enabled, out-of-bounds writes have no effect
    and out-of-bounds reads return zero.
    This is useful for GFX9 where hardware no longer performs
    bounds checking on scratch accesses and hence page faults
    result for out-of-bounds accesses by generated by shaders.
    
    Change-Id: Id2ee4b1f32e70b6bde2541db755727b6a407721b

--- lib/Target/AMDGPU/AMDGPU.h
+++ lib/Target/AMDGPU/AMDGPU.h
@@ -53,6 +53,8 @@ FunctionPass *createSIOptimizeExecMaskingPreRAPass();
 FunctionPass *createSIFixSGPRCopiesPass();
 FunctionPass *createSIBufMemMergePass();
 FunctionPass *createSIMemoryLegalizerPass();
+FunctionPass *createSIInsertScratchBoundsPass();
+FunctionPass *createSIFixScratchSizePass();
 FunctionPass *createSIInsertWaitcntsPass();
 FunctionPass *createSIInsertWaterfallPass();
 FunctionPass *createSIPreAllocateWWMRegsPass();
@@ -149,6 +151,13 @@ extern char &SIWholeQuadModeID;
 
 void initializeSILowerControlFlowPass(PassRegistry &);
 extern char &SILowerControlFlowID;
+
+void initializeSIInsertScratchBoundsPass(PassRegistry &);
+extern char &SIInsertScratchBoundsID;
+
+void initializeSIFixScratchSizePass(PassRegistry &);
+extern char &SIFixScratchSizeID;
+extern const char *const SIScratchSizeSymbol;
 
 void initializeSIInsertSkipsPass(PassRegistry &);
 extern char &SIInsertSkipsPassID;
